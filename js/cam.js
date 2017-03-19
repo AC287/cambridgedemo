a6c43fca7d03d5a4bc7795f6a48c5cef6065a3fc
AC287 <arthurchen287@gmail.com>
2017-03-19 16:04:05 -0400

    tested with different classname.

--- js/cam.js
+++ js/cam.js
@@ -13,10 +13,27 @@ $(document).ready(function(){
   //   console.log(typeof(x));
   // }
 
-  $('.gc-desc-img').mouseenter(function(){
-    console.log($('.gc-desc-img').find());
-    // $('img').hide();
-    // $('p').show();
+  // $('.gc-desc-img').mouseenter(function(){
+  //   console.log($('.gc-desc-img').find());
+  //   // $('img').hide();
+  //   // $('p').show();
+  // })
+  $('.gc-block').mouseenter(function(){
+    var currentClass = '.'+classSplit($(this).attr('class'));
+    // console.log(currentClass);
+    var imgClass = '.'+currentClass+'-desc';
+    $(currentClass + ' img').hide();
+
+  }).mouseleave(function(){
+    var currentClass = '.'+classSplit($(this).attr('class'));
+    // console.log(currentClass);
+    var imgClass = '.'+currentClass+'-desc';
+    $(currentClass + ' img').show();
   })
+
+  function classSplit(x){
+    var mainclass=x.split(' ');
+    return mainclass[0];
+  }
 
 })
