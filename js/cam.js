8d191158d642942358b3a5ac46331e07738e57c0
AC <arthurchen287@gmail.com>
2017-03-19 18:17:17 -0400

    hoverscript success.

--- js/cam.js
+++ js/cam.js
@@ -1,34 +1,19 @@
 $(document).ready(function(){
   console.log('cam.js is loaded');
 
-  // $(".gc-desc-img img").hover(function(){
-  //   console.log($(this).attr('class'));
-  // })
-
-  // $('.gc-desc-img img').mouseenter(function(){
-  //   classSplit($(this).attr('class'));
-  // })
-  //
-  // function classSplit(x) {
-  //   console.log(typeof(x));
-  // }
-
-  // $('.gc-desc-img').mouseenter(function(){
-  //   console.log($('.gc-desc-img').find());
-  //   // $('img').hide();
-  //   // $('p').show();
-  // })
   $('.gc-block').mouseenter(function(){
     var currentClass = '.'+classSplit($(this).attr('class'));
     // console.log(currentClass);
     var imgClass = '.'+currentClass+'-desc';
     $(currentClass + ' img').hide();
+    $(currentClass + '-desc').fadeIn();
 
   }).mouseleave(function(){
     var currentClass = '.'+classSplit($(this).attr('class'));
     // console.log(currentClass);
     var imgClass = '.'+currentClass+'-desc';
-    $(currentClass + ' img').show();
+    $(currentClass + '-desc').hide();
+    $(currentClass + ' img').fadeIn();
   })
 
   function classSplit(x){
