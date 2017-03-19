1e9469ea721b60a31861ad68d2e34a4e84e5ed22
AC287 <arthurchen287@gmail.com>
2017-03-19 14:09:28 -0400

    first testing with js.

--- js/cam.js
+++ js/cam.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){
   console.log('cam.js is loaded');
+
+  // $(".gc-desc-img img").hover(function(){
+  //   console.log($(this).attr('class'));
+  // })
+
+  // $('.gc-desc-img img').mouseenter(function(){
+  //   classSplit($(this).attr('class'));
+  // })
+  //
+  // function classSplit(x) {
+  //   console.log(typeof(x));
+  // }
+
+  $('.gc-desc-img').mouseenter(function(){
+    console.log($('.gc-desc-img').find());
+    // $('img').hide();
+    // $('p').show();
+  })
+
 })
