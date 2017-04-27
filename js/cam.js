e128a5f3f512a5bb9e727632f2aa822f6eb9684d
Arthur Chen <arthurchen@central.com>
2017-04-27 15:01:02 -0400

    testing stuff

--- js/cam.js
+++ js/cam.js
@@ -1,5 +1,24 @@
 $(document).ready(function(){
   console.log('cam.js is loaded');
+  // $(window).resize();
+  // $(window).resize(function(){
+  //   console.log('resizing . . . .');
+  //   if((window.fullscreen) || (window.innerWidth == screen.width && window.innerHeight == screen.height)) {
+  //     $("html").css("overflow","hidden");
+  //   } else {
+  //     $("html").css("overflow", "auto");
+  //   }
+  // });
+  console.log((window.innerWidth == screen.width && window.innerHeight == screen.height))
+  console.log(window.innerWidth);
+  console.log(screen.innerWidth);
+  // console.log(window.fullscreen);
+  // console.log(window.innerWidth);
+  // if((window.fullscreen) || (window.innerWidth == screen.width && window.innerHeight == screen.height)) {
+  //   $("html").css("overflow","hidden");
+  // }
+
+  $('html').css('overflow-x','hidden');
 
   $('[data-toggle="tooltip"]').tooltip();
 
